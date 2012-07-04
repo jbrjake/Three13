fddb351b54598f6fa5b1d2d0f0a4ad70bf802780
Jon Rubin <jb.rubin@gmail.com>
2012-07-04 15:16:08 -0400

    Adds a unit testing target.

---
+++ 313-test/_13_test.h
@@ -0,0 +1,13 @@
+//
+//  _13_test.h
+//  313-test
+//
+//  Created by Jonathon Rubin on 7/4/12.
+//  Copyright (c) 2012 The Nielsen Company. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface _13_test : SenTestCase
+
+@end
---
+++ 313-test/_13_test.m
@@ -0,0 +1,32 @@
+//
+//  _13_test.m
+//  313-test
+//
+//  Created by Jonathon Rubin on 7/4/12.
+//  Copyright (c) 2012 The Nielsen Company. All rights reserved.
+//
+
+#import "_13_test.h"
+
+@implementation _13_test
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in 313-test");
+}
+
+@end
