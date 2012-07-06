4fd1a7b0e9999cc115999b22377efcd3c3aae2ae
Jon Rubin <jb.rubin@gmail.com>
2012-07-06 16:06:26 -0400

    Stubs out protocols for views to use to communicate with the game model.

---
+++ Classes/Three13GameDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Three13GameDelegate.h
+//  313-prototype
+//
+//  Created by Jonathon Rubin on 7/6/12.
+//  Copyright (c) 2012 Jonathon Rubin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol Three13GameDelegate <NSObject>
+
+
+
+@end
---
+++ Classes/Three13ViewDataSource.h
@@ -0,0 +1,13 @@
+//
+//  Three13ViewDataSource.h
+//  313-prototype
+//
+//  Created by Jonathon Rubin on 7/6/12.
+//  Copyright (c) 2012 Jonathon Rubin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol Three13ViewDataSource <NSObject>
+
+@end
