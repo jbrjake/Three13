f36f71f006370cc75cb86225cfed21bf5b10c0b9
Jon Rubin <jb.rubin@gmail.com>
2012-07-31 23:59:09 -0400

    Adds comments describing the Three13GameDelegate protocol.

--- Classes/Three13GameDelegate.h
+++ Classes/Three13GameDelegate.h
@@ -10,8 +10,36 @@
 
 @protocol Three13GameDelegate <NSObject>
 
+/*
+ * @brief Informs the delegate the game has begun
+ * When the delegate view controller has finished responding
+ * (perhaps by animating the game view into place),
+ * it must run the provided completion handler.
+ * This way, the start of the game does not flow immediately into the first round.
+ * @param completionHandler A block the delegate is responsible for calling once it is ready for the game to proceed
+ */
 - (void) respondToStartOfGameWithCompletionHandler:(void (^)())completionHandler;
+
+/*
+ * @brief Informs the delegate a level has ended
+ * When the delegate view controller has finished responding
+ * (perhaps by animating the cards off screen and displaying the current score),
+ * it must run the provided completion handler.
+ * This way, the end of one level does not flow immediately into the start of the next.
+ * @param dict The current game state, from Three13Game's gameDict: method
+ * @param completionHandler A block the delegate is responsible for calling once it is ready for the next level to proceed
+ */
 - (void) respondToEndOfLevelWithDictionary:(NSMutableDictionary*)dict andCompletionHandler:(void (^)())completionHandler;
+
+/*
+ * @brief Informs the delegate a card has been discarded (i.e. the round is over)
+ * When the delegate view controller has finished responding
+ * (perhaps by animating the discarded card to the discard pile),
+ * it must run the provided completion handler.
+ * This way, the end of one round does not flow immediately into the start of the next.
+ * @param dict The current game state, from Three13Game's gameDict: method
+ * @param completionHandler A block the delegate is responsible for calling once it is ready for the next round to proceed
+ */
 - (void) respondToCardBeingDiscardedWithDictionary:(NSMutableDictionary*)dict andCompletionHandler:(void (^)())completionHandler;
 
 @end
