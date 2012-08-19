ff4213e80e81c1fbb461df7b2dd68dc4ecde8002
Jon Rubin <jb.rubin@gmail.com>
2012-08-19 13:51:31 -0400

    Adds players to the game dict.

--- Classes/Three13Game.m
+++ Classes/Three13Game.m
@@ -104,7 +104,7 @@
 }
 
 -(NSMutableDictionary *) gameDict {
-    NSMutableDictionary * dict = [NSMutableDictionary dictionaryWithObjectsAndKeys: [hand cardIDs], @"hand", [deck cardIDs], @"deck", @(knownCard.number), @"known", @(mysteryCard.number), @"mystery", @0, @"discard", @(level), @"level", @(round), @"round", nil ];
+    NSMutableDictionary * dict = [NSMutableDictionary dictionaryWithObjectsAndKeys: players, @"players", [deck cardIDs], @"deck", @(knownCard.number), @"known", @(mysteryCard.number), @"mystery", @0, @"discard", @(level), @"level", @(round), @"round", nil ];
     return dict;
 }
 
