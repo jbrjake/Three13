3b0af45508386b56648b01840fe9e47ae81ad97b
Jon Rubin <jb.rubin@gmail.com>
2013-06-13 10:14:19 -0700

    Removes some of the template cruft, to get a nice white empty view.

--- Three13-sprite-prototype/Three13-sprite-prototype/TTTMyScene.m
+++ Three13-sprite-prototype/Three13-sprite-prototype/TTTMyScene.m
@@ -14,16 +14,8 @@
     if (self = [super initWithSize:size]) {
         /* Setup your scene here */
         
-        self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
+        self.backgroundColor = [SKColor whiteColor];
         
-        SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
-        
-        myLabel.text = @"Hello, World!";
-        myLabel.fontSize = 30;
-        myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-                                       CGRectGetMidY(self.frame));
-        
-        [self addChild:myLabel];
     }
     return self;
 }
