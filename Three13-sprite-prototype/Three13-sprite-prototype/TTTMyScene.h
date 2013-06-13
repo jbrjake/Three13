c9bf2205fe901ce0b7cbf98dc393001987538dd9
Jon Rubin <jb.rubin@gmail.com>
2013-06-13 10:10:03 -0700

    Adds a SpriteKit project to play with

---
+++ Three13-sprite-prototype/Three13-sprite-prototype/TTTMyScene.h
@@ -0,0 +1,13 @@
+//
+//  TTTMyScene.h
+//  Three13-sprite-prototype
+//
+
+//  Copyright (c) 2013 Jonathon Rubin. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface TTTMyScene : SKScene
+
+@end
---
+++ Three13-sprite-prototype/Three13-sprite-prototype/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Three13-sprite-prototype
+//
+//  Created by Jonathon Rubin on 6/13/13.
+//  Copyright (c) 2013 Jonathon Rubin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TTTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TTTAppDelegate class]));
+    }
+}
