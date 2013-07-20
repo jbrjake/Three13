6c8d0b8c5f17a5f83892282b60e6060d36fce5db
Jon Rubin <jb.rubin@gmail.com>
2013-07-20 15:16:45 -0400

    Tests out the meldNode UI. It doesn't really work...too cramped.

--- Three13-sprite-prototype/Three13-sprite-prototype/TTTMyScene.h
+++ Three13-sprite-prototype/Three13-sprite-prototype/TTTMyScene.h
@@ -7,7 +7,14 @@
 //
 
 #import <SpriteKit/SpriteKit.h>
+@class TTTMeldNode;
 
 @interface TTTMyScene : SKScene
+
+@property TTTMeldNode * handMeld;
+@property TTTMeldNode * meld1;
+@property TTTMeldNode * meld2;
+@property TTTMeldNode * meld3;
+@property TTTMeldNode * meld4;
 
 @end
--- Three13-sprite-prototype/Three13-sprite-prototype/TTTMyScene.m
+++ Three13-sprite-prototype/Three13-sprite-prototype/TTTMyScene.m
@@ -7,6 +7,8 @@
 //
 
 #import "TTTMyScene.h"
+#import "TTTCardSpriteNode.h"
+#import "TTTMeldNode.h"
 
 @implementation TTTMyScene
 
@@ -15,27 +17,95 @@
         /* Setup your scene here */
         
         self.backgroundColor = [SKColor whiteColor];
+        _handMeld = [TTTMeldNode nodeWithColor:[SKColor lightGrayColor] andSize:CGSizeMake(175, 175)];
         
+        _meld1 = [TTTMeldNode nodeWithColor:[SKColor lightGrayColor] andSize:CGSizeMake(125, 125)];
+
+        _meld2 = [TTTMeldNode nodeWithColor:[SKColor lightGrayColor] andSize:CGSizeMake(125, 125)];
+    
+        _meld3 = [TTTMeldNode nodeWithColor:[SKColor lightGrayColor] andSize:CGSizeMake(125, 125)];
+        
+        _meld4 = [TTTMeldNode nodeWithColor:[SKColor lightGrayColor] andSize:CGSizeMake(125, 125)];
+        
+        [self setMeldLocations];
+        [self addChild:self.handMeld];
+        [self addChild:self.meld1];
+        [self addChild:self.meld2];
+        [self addChild:self.meld3];
+        [self addChild:self.meld4];
+        
+//        SKAction *action = [SKAction rotateByAngle:M_PI duration:2.5];
+//        [self.handMeld runAction:[SKAction repeatActionForever:action]];
+
+        [self dealHand];
     }
     return self;
 }
 
+-(void) setMeldLocations {
+    self.handMeld.position = CGPointMake(self.frame.size.width/2, self.frame.size.height/2);
+    self.meld1.position = CGPointMake(self.frame.size.width - 75, self.frame.size.height - 75);
+    self.meld2.position = CGPointMake(75, self.frame.size.height - 75);
+    self.meld3.position = CGPointMake(self.frame.size.width - 75, 75);
+    self.meld4.position = CGPointMake(75, 75);
+}
+
+-(void) dealHand {
+    TTTCardSpriteNode * cyanSprite = [TTTCardSpriteNode nodeWithColor:[SKColor cyanColor] andSize:CGSizeMake(44,44)andVertices:3];
+    TTTCardSpriteNode * magentaSprite = [TTTCardSpriteNode nodeWithColor:[SKColor magentaColor] andSize:CGSizeMake(44,44)andVertices:4];
+    TTTCardSpriteNode * yellowSprite = [TTTCardSpriteNode nodeWithColor:[SKColor yellowColor] andSize:CGSizeMake(44,44)andVertices:5];
+    TTTCardSpriteNode * blackSprite = [TTTCardSpriteNode nodeWithColor:[SKColor blackColor] andSize:CGSizeMake(44,44)andVertices:6];
+    
+    [self.handMeld addChild:cyanSprite];
+    [self.handMeld addChild:magentaSprite];
+    [self.handMeld addChild:yellowSprite];
+    [self.handMeld addChild:blackSprite];
+/*
+    SKAction *action = [SKAction rotateByAngle:M_PI duration:2.5];
+    [cyanSprite runAction:[SKAction repeatActionForever:action]];
+    [magentaSprite runAction:[SKAction repeatActionForever:action]];
+    [yellowSprite runAction:[SKAction repeatActionForever:action]];
+    [blackSprite runAction:[SKAction repeatActionForever:action]];
+*/
+    [self.handMeld positionCards];
+
+}
+
+
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     /* Called when a touch begins */
-    
-    for (UITouch *touch in touches) {
-        CGPoint location = [touch locationInNode:self];
-        
-        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
-        
-        sprite.position = location;
-        
-        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
-        
-        [sprite runAction:[SKAction repeatActionForever:action]];
-        
-        [self addChild:sprite];
+    if (self.handMeld.children.count == 14) {
+        return;
     }
+    
+    NSArray * colors = @[ [SKColor cyanColor], [SKColor magentaColor], [SKColor yellowColor], [SKColor blackColor] ];
+    for (UITouch *touch in touches) {
+        
+        TTTCardSpriteNode * sprite = [TTTCardSpriteNode nodeWithColor:colors[rand()%4] andSize:CGSizeMake(44,44)andVertices:rand()%11+3];
+        SKAction *action = [SKAction rotateByAngle:M_PI duration:2.5];
+        
+      //  [sprite runAction:[SKAction repeatActionForever:action]];
+        
+        [self.handMeld addChild:sprite];
+        [self.handMeld positionCards];
+    }
+/*
+    NSInteger childCount = self.handMeld.children.count;
+    NSInteger pos1 = rand()%childCount;
+    NSInteger pos2 = rand()%childCount;
+    TTTCardSpriteNode * card1 = self.handMeld.children[pos1];
+    TTTCardSpriteNode * card2 = self.handMeld.children[pos2];
+    [self.handMeld removeChildrenInArray:@[card1, card2]];
+    if (pos1 == childCount-1) {
+        pos1--;
+    }
+    if (pos2 == childCount-1) {
+        pos2--;
+    }
+    [self.handMeld insertChild:card2 atIndex:pos1];
+    [self.handMeld insertChild:card1 atIndex:pos2];
+    [self.handMeld positionCards];
+*/
 }
 
 -(void)update:(CFTimeInterval)currentTime {
