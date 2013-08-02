b50d17a54632fd668cee386ff1fce90c68a09440
Jon Rubin <jb.rubin@gmail.com>
2013-08-01 21:25:28 -0400

    Converts to XCTest.

--- Three13Prototype/313-test/_13_test.h
+++ Three13Prototype/313-test/_13_test.h
@@ -3,11 +3,11 @@
 //  313-test
 //
 //  Created by Jonathon Rubin on 7/4/12.
-//  Copyright (c) 2012 The Nielsen Company. All rights reserved.
+//  Copyright (c) 2012 Jonathon Rubin. All rights reserved.
 //
 
-#import <SenTestingKit/SenTestingKit.h>
+#import <XCTest/XCTest.h>
 
-@interface _13_test : SenTestCase
+@interface _13_test : XCTestCase
 
 @end
